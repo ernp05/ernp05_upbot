a08b255fb5b8a0795bdb2f0c802a7fab51e1e9e7
ernp05 <104167288+ernp05@users.noreply.github.com>
2022-04-25 15:50:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -244,7 +244,7 @@ def onmessage(update,bot:ObigramClient):
         tl_admin_user = os.environ.get('tl_admin_user')
 
         #set in debug
-        tl_admin_user = 'obisoftdev'
+        tl_admin_user = 'ERNP05'
 
         jdb = JsonDatabase('database')
         jdb.check_create()
